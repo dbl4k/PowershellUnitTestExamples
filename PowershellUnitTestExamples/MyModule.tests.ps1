b1c0f19427be0c83fec872c7e6a2b5b3bae1c174
dbl4k <github@dbl4k.net>
2017-07-04 22:02:41 +0100

    add tests for get-lastname

--- PowershellUnitTestExamples/MyModule.tests.ps1
+++ PowershellUnitTestExamples/MyModule.tests.ps1
@@ -8,13 +8,19 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 Import-Module "$here\MyModule.psm1"
 
 Describe "Get-FirstName" {
-
 	It "Returns 'Dude'" {
 		Get-FirstName | Should Be "Dude"
 	}
-
 	It "Does Not Return 'Duder'" {
 		Get-FirstName | Should Not Be "Duder"
 	}
+}
 
+Describe "Get-LastName" {
+	It "Returns 'McDude'" {
+		Get-LastName | Should Be "McDude"
+	}
+	It "Does Not Return 'McDudeypants'" {
+		Get-LastName | Should Not Be "McDudeypants"
+	}
 }
