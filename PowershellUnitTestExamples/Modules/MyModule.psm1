732635ad66536922142a8821527e485dbf9f2f64
Andrew Bartlett <andrewbartlett@phs.co.uk>
2017-07-05 10:47:23 +0100

    added pipeline input arg to add-days

--- PowershellUnitTestExamples/Modules/MyModule.psm1
+++ PowershellUnitTestExamples/Modules/MyModule.psm1
@@ -13,15 +13,13 @@ function Get-LastName {
 function Add-Days() {
 	# TODO: Would be useful to add pipeline input here.
 	##[cmdletbinding()]
-  	Param (
-  		[parameter(ValueFromPipeline)] 
-			[datetime]$Value,
-  		[parameter()]
-			[int]$DaysToAdd
-  	)
-	Process {
-		return $Value.AddDays($DaysToAdd)
-	}
+	[CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline, Mandatory)]$Value,
+        $DaysToAdd
+    )
+
+	return ([datetime]($Value)).AddDays($DaysToAdd)
 }
 
 function Add-Numbers($first, $second) {
